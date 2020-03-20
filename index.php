6143de6c3725cbee911f56cf0d3f287e191ff6bd
Grognard-dev <lefevre.gregoire19@gmail.com>
2020-03-20 17:30:43 +0100

    nouveau gif et changement

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
         </div>
     </div>
     <?php include "footer.php"?>
-     <div id="conteneur"><div id="bouge"><img src="image/giphy.gif" alt=""></div></div>
+     <div class="banane"><img src="image/simplon.gif" alt=""></div>
     <script src="js/jssor.slider-27.5.0.min.js" type="text/javascript"></script>
     <script type="text/javascript">
         jssor_1_slider_init = function() {
@@ -159,7 +159,7 @@
     </script>
      <script src="js/konami.js"></script>
     <script>var easter_egg = new Konami(function() {
-        var conteneur = document.querySelector("#conteneur")
+        var conteneur = document.querySelector(".banane")
         conteneur.style.display="block"
     });</script>
 </body>
